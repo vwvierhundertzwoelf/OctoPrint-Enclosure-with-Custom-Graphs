d86686373ca6fc290a1dec0ea5f6c1b7c4fab875
Marius Meiswinkel <marius.meiswinkel@inside-m2m.de>
2021-08-09 15:58:06 +0200

    Fixed DHT22 Temp Sensor on Rasberry PI 4

--- octoprint_enclosure/getDHTTemp.py
+++ octoprint_enclosure/getDHTTemp.py
@@ -1,23 +1,39 @@
 import sys
-import Adafruit_DHT
-
+import adafruit_dht
+from board import D2 #my sensor is connected to D19 on RPi. adjust your pin accordingly
+import time
 
 # Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-    '22': Adafruit_DHT.DHT22,
-        '2302': Adafruit_DHT.AM2302 }
+sensor_args = { '11': adafruit_dht.DHT11,
+    '22': adafruit_dht.DHT22}
+#        '2302': Adafruit_DHT.AM2302 } depreciated
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
+    pin = sys.argv[2] #ignoring this for testing purposes
 else:
     sys.exit(1)
 
+dht_device = adafruit_dht.DHT22(D2)
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin,2,0.5)
+read_attempts = 0
+while read_attempts < 7: # try for a reading 7 times since dht's are unreliable
+       try:
+               humidity = dht_device.humidity
+               temperature = dht_device.temperature
 
-if humidity is not None and temperature is not None:
-    print('{0:0.1f} | {1:0.1f}'.format(temperature, humidity))
-else:
-    print('-1 | -1')
+               if humidity is not None and temperature is not None:
+                   print('{0:0.1f} | {1:0.1f}'.format(temperature, humidity))
+                   sys.exit(1)
+       except RuntimeError as error:
+               read_attempts += 1
+               time.sleep(2.0)
+               continue
+       except Exception as error:
+               read_attempts += 1
+               dht_device.exit()
+               raise error
 
+       time.sleep(2.0)
+
+print('-1 | -1')
 sys.exit(1)
