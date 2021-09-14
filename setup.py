ed882b0ce494de7919694bda9c200839c451c264
Marius Meiswinkel <marius.meiswinkel@inside-m2m.de>
2021-09-14 10:13:44 +0200

    Change Version number

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ plugin_package = "octoprint_enclosure"
 plugin_name = "OctoPrint-Enclosure"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "4.13.1"
+plugin_version = "4.13.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = "Awesome plugin to control everything that you ever wanted on your enclosure"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Vitor Henrique"
+plugin_author = "Vitor Henrique, Marius Meiswinkel"
 
 # The plugin's author's mail address.
 plugin_author_email = "vitormhenrique@gmail.com"
